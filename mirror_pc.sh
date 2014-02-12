6a5c834b6461ac320ce3b21214c15daedff7f608
Hunter Hu <hunterhu.ca@gmail.com>
2014-02-12 13:08:57 -0600

    Always mirror both bsp and rootfs
    Since if there was no change, there will be no mirroring.
    
    Simplifies the command.

--- mirror_pc.sh
+++ mirror_pc.sh
@@ -4,13 +4,14 @@
 # 1> take rsync type argument, sunxi-bsp or rootfs-projects, or even more generic
 # 2> take server IP address
 
-SERVER_IP="10.200.146.32"
+SERVER_IP="192.168.1.101"
 
 BSP_SRC="/home/hunter/projects/sunxi-bsp/"
-BSP_DEST="root@$SERVER_IP:/home/core/projects/sunxi-bsp/"
+BSP_DEST="root@$SERVER_IP:/home/hunter/projects/sunxi-bsp/"
 
 ROOTFS_SRC="/home/hunter/rootfs-projects/"
-ROOTFS_DEST="root@$SERVER_IP:/home/core/rootfs-projects/"
+ROOTFS_DEST="root@$SERVER_IP:/home/hunter/rootfs-projects/"
+
 
 #mirror macbook pro sunxi-bsp to the server
 echo "Mirroring $BSP_SRC to $BSP_DEST ... ..."
