cb028e862db88bc1dc9c1141280036b034a1163a
Hunter Hu <hunterhu.ca@gmail.com>
2014-02-14 09:59:47 -0600

    Mirror scripts:consolidate to projects directory

--- mirror_pc.sh
+++ mirror_pc.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-#TODO
-# 1> take rsync type argument, sunxi-bsp or rootfs-projects, or even more generic
-# 2> take server IP address
+PC_IP="192.168.1.130"
 
-SERVER_IP="192.168.1.101"
+PRJ_SRC="/home/hunter/projects/"
+PRJ_DEST="root@$PC_IP:/home/hunter/projects/"
 
-BSP_SRC="/home/hunter/projects/sunxi-bsp/"
-BSP_DEST="root@$SERVER_IP:/home/hunter/projects/sunxi-bsp/"
-
-ROOTFS_SRC="/home/hunter/rootfs-projects/"
-ROOTFS_DEST="root@$SERVER_IP:/home/hunter/rootfs-projects/"
-
-
-#mirror macbook pro sunxi-bsp to the server
-echo "Mirroring $BSP_SRC to $BSP_DEST ... ..."
-sudo rsync -avxHAXW --progress --delete $BSP_SRC $BSP_DEST
-
-#mirror macbook pro rootfs-projects to the server
-echo "Mirroring $ROOTFS_SRC to $ROOTFS_DEST ... ..."
-sudo rsync -avxHAXW --progress --delete $ROOTFS_SRC $ROOTFS_DEST
+echo "Mirroring $PRJ_SRC to $PRJ_DEST ... ..."
+sudo rsync -avxHAXW --progress --delete $PRJ_SRC $PRJ_DEST
--- mirror_server.sh
+++ mirror_server.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-#TODO
-# 1> take rsync type argument, sunxi-bsp or rootfs-projects, or even more generic
-# 2> take server IP address
-
 SERVER_IP="10.200.146.32"
 
-BSP_SRC="/home/hunter/projects/sunxi-bsp/"
-BSP_DEST="root@$SERVER_IP:/home/core/projects/sunxi-bsp/"
+PRJ_SRC="/home/hunter/projects/"
+PRJ_DEST="root@$SERVER_IP:/home/core/projects/"
 
-ROOTFS_SRC="/home/hunter/rootfs-projects/"
-ROOTFS_DEST="root@$SERVER_IP:/home/core/rootfs-projects/"
+#mirror macbook pro ~/projects to the server
+echo "Mirroring $PRJ_SRC to $PRJ_DEST ... ..."
+sudo rsync -avxHAXW --progress --delete $PRJ_SRC $PRJ_DEST
 
-#mirror macbook pro sunxi-bsp to the server
-echo "Mirroring $BSP_SRC to $BSP_DEST ... ..."
-sudo rsync -avxHAXW --progress --delete $BSP_SRC $BSP_DEST
-
-#mirror macbook pro rootfs-projects to the server
-echo "Mirroring $ROOTFS_SRC to $ROOTFS_DEST ... ..."
-sudo rsync -avxHAXW --progress --delete $ROOTFS_SRC $ROOTFS_DEST
