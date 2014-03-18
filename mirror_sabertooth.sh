0c6db8d416be282b9a396de938871aa99fc65935
Hunter Hu <hunterhu.ca@gmail.com>
2014-03-18 17:31:17 -0500

    Rename mirror_server.sh to mirror_sabertooth.sh

--- mirror_sabertooth.sh
+++ mirror_sabertooth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SERVER_IP="10.200.146.32"
+SABERTOOTH_IP="10.200.146.32"
 
 PRJ_SRC="/home/hunter/projects/"
-PRJ_DEST="root@$SERVER_IP:/home/core/projects/"
+PRJ_DEST="root@$SABERTOOTH_IP:/home/core/projects/"
 
 #mirror macbook pro ~/projects to the server
 echo "Mirroring $PRJ_SRC to $PRJ_DEST ... ..."
