462743750fcfa1073d06a662438e83421fb3d025
Hunter Hu <hunterhu.ca@gmail.com>
2014-07-12 23:08:51 -0500

    Remove olinuxino

--- update-nand-contents.sh
+++ update-nand-contents.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 pause() {
-	sleep 2
+	sleep 4
 }
 
 SRC="./build/a20-olinuxino_micro_hwpack/kernel/uImage"
-DST="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/uImage.olinuxino"
+DST="$HOME/projects/nand-contents-container/nand-contents/uImage"
 
 SRC_LIB="./build/a20-olinuxino_micro_hwpack/rootfs/lib"
-DST_LIB="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/rootfs"
+DST_LIB="$HOME/projects/nand-contents-container/nand-contents/rootfs"
 
 #Updating uImage
 echo ""
-echo "Updating \"$DST\" from \"$SRC\" ..."
+echo "Updating uImage ..."
+echo "From: $SRC"
+echo "  To: $DST"
+echo ""
 pause
 sudo cp $SRC $DST
 
 #Updating kernel modules
-echo "Removing old \"$DST_LIB/lib\" ..."
+echo "Removing old modules ..."
+echo "From: $DST_LIB/lib/"
+echo ""
 pause
 sudo rm -rf $DST_LIB/lib/
 
-echo "Updating \"$DST_LIB\" from \"$SRC_LIB\" ..."
+echo "Copying new modules ..."
+echo "From: $SRC_LIB"
+echo "  To: $DST_LIB"
 pause
 sudo cp -R $SRC_LIB $DST_LIB
 echo "DONE"
