b4bab574b4e2dedfaf407be2d7c7101ee1464044
Hunter Hu <hunterhu.ca@gmail.com>
2014-07-12 22:09:28 -0500

    update-nand-content.sh:use one board olinuxino-a20

--- update-nand-contents.sh
+++ update-nand-contents.sh
@@ -1,55 +1,14 @@
 #!/bin/bash
 
-usage() {
-	echo ""
-	echo "$0: <boardname>"
-	echo "    <boardname>: \"olinuxino\" or \"som\""
-	echo ""
-}
-
 pause() {
 	sleep 2
 }
 
-if [ "$#" -lt "1" ]; then
-	usage
-	exit 1
-fi
+SRC="./build/a20-olinuxino_micro_hwpack/kernel/uImage"
+DST="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/uImage.olinuxino"
 
-PWD=`pwd`
-
-if [ "$1" == "olinuxino" ]; then
-  if [ "$PWD" != "$HOME/projects/olinuxino-a20-sunxi-dev/sunxi-bsp" ]; then
-	echo ""
-	echo "Wrong directory detected, you requested \"$1\", but we are in \"$PWD\" ..."
-	echo ""
-	exit 1
-  fi
-
-  SRC="./build/a20-olinuxino_micro_hwpack/kernel/uImage"
-  DST="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/uImage.olinuxino"
-
-  SRC_LIB="./build/a20-olinuxino_micro_hwpack/rootfs/lib"
-  DST_LIB="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/rootfs"
-
-elif [ "$1" == "som" ]; then
-  if [ "$PWD" != "$HOME/projects/som-evb-a20-sunxi-dev/sunxi-bsp" ]; then
-    echo ""
-	echo "Wrong directory detected, you requested \"$1\", but we are in \"$PWD\" ..."
-	echo ""
-	exit 1
-  fi
-
-  SRC="./build/a20-som_hwpack/kernel/uImage"
-  DST="$HOME/projects/nand-contents-container/nand-contents/som-a20/uImage.som"
-
-  SRC_LIB="./build/a20-som_hwpack/rootfs/lib"
-  DST_LIB="$HOME/projects/nand-contents-container/nand-contents/som-a20/rootfs"
-
-else
-	usage
-	exit 1
-fi
+SRC_LIB="./build/a20-olinuxino_micro_hwpack/rootfs/lib"
+DST_LIB="$HOME/projects/nand-contents-container/nand-contents/olinuxino-a20/rootfs"
 
 #Updating uImage
 echo ""
