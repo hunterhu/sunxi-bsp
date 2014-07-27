6b8fac74ef8ec3d5021c447621e673299d3de0f6
Hunter Hu <hunterhu.ca@gmail.com>
2014-07-27 13:10:12 -0500

    Add boot.cmd file

---
+++ boot.cmd
@@ -0,0 +1,4 @@
+setenv bootargs console=ttyS0,115200 console=tty0 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1024x768p60 root=/dev/mmcblk0p2 rootwait panic=10 ${extra}
+fatload mmc 0 0x43000000 script.bin
+fatload mmc 0 0x48000000 uImage
+bootm 0x48000000
