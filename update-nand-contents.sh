da9746b89e6af7dc544bf2916528380d5e2c5f28
Hunter Hu <hunterhu.ca@gmail.com>
2014-10-01 22:57:16 -0500

    update-nand-contents.sh:update nand-contents fex
    After copying the script.bin.lcd4.3, generate nand-contents/
    script.lcd4.3.fex and diff with sunxi-boards version so that
    they are identical.

--- update-nand-contents.sh
+++ update-nand-contents.sh
@@ -18,9 +18,12 @@ DST_UIMAGE="$DST/uImage"
 
 SRC_SCRIPT="$SRC/kernel/script.bin"
 DST_SCRIPT="$DST/script.bin.lcd4.3"
+DST_FEX="$DST/script.lcd4.3.fex"
 
 SRC_LIB="$SRC/rootfs/lib"
 DST_LIB="$DST/rootfs/lib"
+
+SUNXI_BOARDS_FEX="sunxi-boards/sys_config/a20/a20-som-paydia.fex"
 
 echo ""
 echo "Updating uImage ..."
@@ -36,6 +39,12 @@ echo "  To: $DST_SCRIPT"
 echo ""
 pause
 sudo cp $SRC_SCRIPT $DST_SCRIPT
+echo "$DST/bin2fex $DST_SCRIPT $DST_FEX"
+sudo $DST/bin2fex $DST_SCRIPT $DST_FEX
+echo "-------------------->"
+echo "diff $SUNXI_BOARDS_FEX $DST_FEX"
+diff $SUNXI_BOARDS_FEX $DST_FEX
+echo "<--------------------"
 
 echo "Removing old modules ..."
 echo "From: $DST_LIB"
