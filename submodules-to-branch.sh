19c8968a87eed0bf65c497f68e2fffa4bf00b75e
Hunter Hu <hunterhu.ca@gmail.com>
2014-10-20 12:03:19 -0500

    submodules-to-branch.sh:print current branches
    If no target branch is given.

--- submodules-to-branch.sh
+++ submodules-to-branch.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+submodules=( linux-sunxi u-boot-sunxi sunxi-boards )
+
 clear
 if [ "" == "$1" ]; then
-        echo "which branch to switch to?"
-        exit 1
-fi
+    for repo in "${submodules[@]}"
+    do
+            echo "==== \"$repo\" ===="
+            cd ./$repo
+            git branch
+            cd ..
+            echo ""
+    done
 
-submodules=( linux-sunxi u-boot-sunxi sunxi-boards )
+    echo "=========================="
+    echo "which branch to switch to?"
+    echo "=========================="
+    exit 1
+fi
 
 for repo in "${submodules[@]}"
 do
